9db92824b8a3048816258d76a4dd838f33001836
Tim Watkins <timothy.watkins@gmail.com>
2018-09-13 18:28:04 -0600

    working on https

--- components/express_webserver.js
+++ components/express_webserver.js
@@ -5,39 +5,49 @@ const querystring = require('querystring');
 const http = require('http');
 const hbs = require('express-hbs');
 
+const https = require('https');
+/*const privateKey  = fs.readFileSync('server.key', 'utf8');
+const certificate = fs.readFileSync('server.crt', 'utf8');
+const credentials = {key: privateKey, cert: certificate};*/
+
 module.exports = function(controller) {
-  const webserver = express();
-  webserver.use(function(req, res, next) {
+  const app = express();
+  app.use(function(req, res, next) {
     req.rawBody = '';
     req.on('data', function(chunk) {
       req.rawBody += chunk;
     });
     next();
   });
-  webserver.use(cookieParser());
-  webserver.use(bodyParser.json());
-  webserver.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cookieParser());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
   // set up handlebars ready for tabs
-  webserver.engine('hbs', hbs.express4({partialsDir: __dirname + '/../views/partials'}));
-  webserver.set('view engine', 'hbs');
-  webserver.set('views', __dirname + '/../views/');
+  app.engine('hbs', hbs.express4({partialsDir: __dirname + '/../views/partials'}));
+  app.set('view engine', 'hbs');
+  app.set('views', __dirname + '/../views/');
   
-  webserver.use(express.static('public'));
+  app.use(express.static('public'));
+  app.get('/health-check', (req, res) => res.sendStatus(200));
 
-  const server = http.createServer(webserver);
-  server.listen(process.env.PORT || 3000, null, function() {
-    console.log('Express webserver configured and listening at http://localhost:' + process.env.PORT || 3000);
+  const server = http.createServer(app);
+  /*const servers = https.createServer(credentials, app);*/
+  server.listen(3000, null, function() {
+    console.log('Express webserver configured and listening at http://localhost:3000');
   });
+  /*servers.listen(3443, null, function() {
+    console.log('Express webserver configured and listening at https://localhost:3443');
+  });*/
 
   // import all the pre-defined routes that are present in /components/routes
   const normalizedPath = require("path").join(__dirname, "routes");
   require("fs").readdirSync(normalizedPath).forEach(function(file) {
-    require("./routes/" + file)(webserver, controller);
+    require("./routes/" + file)(app, controller);
   });
 
-  controller.webserver = webserver;
+  controller.webserver = app;
   controller.httpserver = server;
 
-  return webserver;
+  return app;
 }
