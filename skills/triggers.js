c3d94be0a47398c639131f84991275470f48b0fd
Tim Watkins <timothy.watkins@gmail.com>
2018-09-13 20:53:26 -0600

    hummus

--- skills/triggers.js
+++ skills/triggers.js
@@ -1,5 +1,5 @@
 module.exports = function(controller) {
-  controller.hears('hummus', [], function (bot, message) {
+  controller.hears('hummus', ['direct_message', 'direct_mention', 'mention', 'ambient'], function (bot, message) {
     bot.say({
       text: 'https://i.imgur.com/Tm9GN74.png',
       channel: message.channel,
