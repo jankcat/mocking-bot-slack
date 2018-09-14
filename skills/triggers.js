4d4947e8f755cabe233d0999c829fbf45227d080
Tim Watkins <timothy.watkins@gmail.com>
2018-09-13 20:54:04 -0600

    constd

--- skills/triggers.js
+++ skills/triggers.js
@@ -1,5 +1,7 @@
 module.exports = function(controller) {
-  controller.hears('hummus', ['direct_message', 'direct_mention', 'mention', 'ambient'], function (bot, message) {
+  const message_types = ['direct_message', 'direct_mention', 'mention', 'ambient'];
+  
+  controller.hears('hummus', message_types, function (bot, message) {
     bot.say({
       text: 'https://i.imgur.com/Tm9GN74.png',
       channel: message.channel,
